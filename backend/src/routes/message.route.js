1a0406f4364a655cb1a29380ae55b695d38eb284
neeraj kushwaha <workwithneeraj.01@gmail.com>
2025-06-06 09:14:52 +0530

    2 changes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { protectedRoutes } from '../middleware/ProtectedRoute.js';
+import { validateObjectId } from '../middleware/validateObjectId.js';
 import { getUserForSidebar } from '../controllers/message.controller.js';
 import { getMessages } from '../controllers/message.controller.js';
 import { sendMessages } from '../controllers/message.controller.js';
 const router = express.Router();
 
 
-router.get('/user',protectedRoutes,getUserForSidebar);
-router.get('/:id',protectedRoutes,getMessages)
-router.post('/send/:id',protectedRoutes,sendMessages)
 
+router.get('/user', protectedRoutes, getUserForSidebar);
+router.get('/:id', protectedRoutes, validateObjectId, getMessages); // Add middleware
+router.post('/send/:id', protectedRoutes, validateObjectId, sendMessages); // Add middleware
 
 export default router;
